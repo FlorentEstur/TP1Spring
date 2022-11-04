9ab5e4d72ca0e2c56eb308e9bf246edc0c72c9a3
jonathan <jonathan@BKE592H>
2022-11-04 15:08:53 +0100

    manytomany produit

--- src/main/java/com/inti/model/Produit.java
+++ src/main/java/com/inti/model/Produit.java
@@ -1,8 +1,15 @@
 package com.inti.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,6 +22,18 @@ public class Produit {
 	
 	@Id
 	private int reference;
-	private String type, dateFabrication, dateExpiration;
+	private String type; 
+	private String dateFabrication; 
+	private String dateExpiration;
 
-}
+	
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(name = "Produit_Restaurant",joinColumns = @JoinColumn(name = "Produit"),
+	inverseJoinColumns = @JoinColumn(name = "Restaurant"))
+	private List<Restaurant> restaurant;
+} 
+//
+//@ManyToMany(cascade = CascadeType.ALL)
+//@JoinTable(name = "Etudiant_Professeur", joinColumns = @JoinColumn(name = "Etudiant"), inverseJoinColumns = @JoinColumn(name = "Professeur"))
+//private List<Professeur> professeur;
+//
