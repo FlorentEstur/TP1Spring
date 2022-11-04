997700588360dd5ab896648d7fa94d31f995514e
FlorentEstur <warcr@DESKTOP-3VUL9G4.home>
2022-11-04 15:03:21 +0100

    Création des sous-classes campagne et ville ainsi que la relation ManyToMany de restaurant et produit

--- src/main/java/com/inti/model/Restaurant.java
+++ src/main/java/com/inti/model/Restaurant.java
@@ -1,7 +1,12 @@
 package com.inti.model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -14,7 +19,10 @@ import lombok.NoArgsConstructor;
 public class Restaurant {
 
 	@Id
-	private int numero;
-	private int telephone;
-	int barbie;
+	protected int numero;
+	protected int telephone;
+	
+	@ManyToMany
+	@JoinTable(name ="Produit_Restaurant", joinColumns = @JoinColumn (name="numero"), inverseJoinColumns = @JoinColumn(name="reference"))
+	private List<Produit> listeProduit;
 }
